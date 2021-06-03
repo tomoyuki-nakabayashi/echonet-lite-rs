423ec5182e8cdf2f4f0f7632ba6c7f6f568d5320
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2021-06-04 06:11:08 +0900

    add service code.

--- src/el_packet.rs
+++ src/el_packet.rs
@@ -1,31 +1,61 @@
+#![allow(dead_code)]
+use num_derive::FromPrimitive;
 
 // TODO: serialize / deserialize
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct ElPacket {
     ehd1: u8,
     ehd2: u8,
     transaction_id: u16,
-    seoj: [u8;3],
-    deoj: [u8;3],
-    esv: u8,
+    seoj: [u8; 3],
+    deoj: [u8; 3],
+    esv: ServiceCode,
     opc: u8,
     props: Box<[u8]>,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, FromPrimitive)]
+#[repr(u8)]
+enum ServiceCode {
+    SetISNA = 0x50,
+    SetCSNA = 0x51,
+    GetSNA = 0x52,
+    INFSNA = 0x53,
+    SetGetSNA = 0x5E,
+    SetI = 0x60,
+    SetC = 0x61,
+    Get = 0x62,
+    INFREQ = 0x63,
+    SetGet = 0x6E,
+    SetRes = 0x71,
+    GetRes = 0x72,
+    INF = 0x73,
+    INFC = 0x74,
+    INFCRes = 0x7A,
+    SetGetRes = 0x7E,
+}
+
 // TODO: builder to make an object easily
-#[derive(Default)]
+#[derive(Debug)]
 struct ElPacketBuilder {
     transaction_id: u16, // builder 作るときに渡しても良いかも
-    seoj: [u8;3],
-    deoj: [u8;3],
-    esv: u8,
+    seoj: [u8; 3],
+    deoj: [u8; 3],
+    esv: Option<ServiceCode>,
     opc: u8,
     props: Box<[u8]>,
 }
 
 impl ElPacketBuilder {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            transaction_id: 0,
+            seoj: [0u8; 3],
+            deoj: [0u8; 3],
+            esv: None,
+            opc: 0,
+            props: Default::default(),
+        }
     }
 
     pub fn transaction_id(mut self, tid: u16) -> Self {
@@ -33,18 +63,18 @@ impl ElPacketBuilder {
         self
     }
 
-    pub fn seoj(mut self, seoj: [u8;3]) -> Self {
+    pub fn seoj(mut self, seoj: [u8; 3]) -> Self {
         self.seoj = seoj;
         self
     }
 
-    pub fn deoj(mut self, deoj: [u8;3]) -> Self {
+    pub fn deoj(mut self, deoj: [u8; 3]) -> Self {
         self.deoj = deoj;
         self
     }
 
-    pub fn esv(mut self, esv: u8) -> Self {
-        self.esv = esv;
+    pub fn esv(mut self, esv: ServiceCode) -> Self {
+        self.esv = Some(esv);
         self
     }
 
@@ -60,7 +90,7 @@ impl ElPacketBuilder {
             transaction_id: self.transaction_id,
             seoj: self.seoj,
             deoj: self.deoj,
-            esv: self.esv,
+            esv: self.esv.unwrap(), // TODO: define error
             opc: self.opc,
             props: self.props,
         }
@@ -69,16 +99,13 @@ impl ElPacketBuilder {
 
 #[cfg(test)]
 mod test {
-    use super::{ElPacket, ElPacketBuilder};
+    use super::*;
 
     #[test]
     fn el() {
-        let packet = ElPacket::default();
-        println!("{:?}", packet);
-
         let packet = ElPacketBuilder::new()
             .transaction_id(1)
-            .esv(0x62)
+            .esv(ServiceCode::Get)
             .build();
         println!("{:?}", packet);
         assert!(true);
