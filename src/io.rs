b3e0901b30de74647aa7419d314ab3c2328e6c94
tomoyuki nakabayashi <tnakabayashi@nature.global>
2022-03-30 08:30:55 +0900

    use core2 because bare-io is yanked.

--- src/io.rs
+++ src/io.rs
@@ -1,9 +1,9 @@
-//! Use std::io from `std` when available, otherwise use bare_io as io.
+//! Use std::io from `std` when available, otherwise use core2 as io.
 
 pub use self::imp::{Error, ErrorKind, Read, Result, Write};
 
 #[cfg(not(feature = "std"))]
-use bare_io as imp;
+use core2 as imp;
 
 #[cfg(feature = "std")]
 use crate::lib::io as imp;
